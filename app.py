8f3b6ecd2b756e60dadc07af2612d2bc49da0ab8
MastanM <41544446+MastanM@users.noreply.github.com>
2021-02-25 14:16:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    html = "<h3>Welcome to Cloud4C !! CICD Automation with Google Cloud Native Tools !!</h3>"
+    html = <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>Cloud4c</title>
+   
+<style>
+    h3
+        {
+            color:blue;
+        }
+    #s
+    {
+            height:50px;
+            width: 1200px;
+    }
+   
+</style>
+</head>
+<body>
+    <div style="background-color:hsl(241, 88%, 51%)" id='s'>
+        <h1 style='color:white;'> Cloud4c </h1>
+    </div>
+   
+    <h3>Welcome to Cloud4C !! CICD Automation with Google Cloud Native Tools !!</h3>
+   
+</body>
+</html>
     return html
 
 if __name__ == "__main__":
